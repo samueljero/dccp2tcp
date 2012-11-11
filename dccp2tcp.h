afe4574b894da1c172ae694503b323c599bc67dc
Samuel Jero <sj323707@ohio.edu>
2012-11-11 16:03:07 -0500

    Add GNU GPL headers to all files

--- dccp2tcp.h
+++ dccp2tcp.h
@@ -1,10 +1,24 @@
 /******************************************************************************
-Author: Samuel Jero
+Utility to convert a DCCP flow to a TCP flow for DCCP analysis via
+		tcptrace.
 
+Copyright (C) 2012  Samuel Jero <sj323707@ohio.edu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Author: Samuel Jero <sj323707@ohio.edu>
 Date: 11/2012
-
-Description: Header file for program to convert a DCCP flow to a TCP flow for DCCP
- 	 	 analysis via tcptrace.
 
 Notes:
 	1)CCID2 ONLY
--- encap.c
+++ encap.c
@@ -1,10 +1,31 @@
 /******************************************************************************
-Author: Samuel Jero
+Utility to convert a DCCP flow to a TCP flow for DCCP analysis via
+		tcptrace. Encapsulation Functions for DCCP conversion to TCP.
 
+Copyright (C) 2012  Samuel Jero <sj323707@ohio.edu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Author: Samuel Jero <sj323707@ohio.edu>
 Date: 11/2012
 
-Description: Encapsulation Functions for DCCP conversion to TCP
-
+Notes:
+	1)CCID2 ONLY
+	2)DCCP MUST use 48 bit sequence numbers
+	3)Checksums are not computed (they are zeroed)
+	4)DCCP DATA packets are not implemented (Linux doesn't use them)
+	5)DCCP Ack packets show up as TCP packets containing one byte
 ******************************************************************************/
 #include "dccp2tcp.h"
 #include "encap.h"
