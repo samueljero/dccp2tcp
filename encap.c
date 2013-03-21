7023edd63c46fb9070be94d5a5608fe465907eb1
Samuel Jero <sj323707@ohio.edu>
2013-03-21 14:01:33 -0400

    Look for IPv4 AND IPv6 in captures with RAW link layers

--- encap.c
+++ encap.c
@@ -41,7 +41,9 @@ int do_encap(int link, struct packet *new, const struct const_packet *old)
 		case DLT_RAW:
 				/*Raw. Just IP*/
 				if(!ipv4_encap(new, old)){
+					if(!ipv6_encap(new,old)){
 						return 0;
+					}
 				}
 				break;
 		case DLT_LINUX_SLL:
